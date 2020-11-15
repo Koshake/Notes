e1dbd5a735b554b50ce4d47f5038859be5a12f26
Koshake <koshake1@mail.com>
2020-11-15 21:51:08 +0300

    first project

--- app/src/main/java/com/koshake1/notes/MainActivity.kt
+++ app/src/main/java/com/koshake1/notes/MainActivity.kt
@@ -2,10 +2,22 @@ package com.koshake1.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragment = MainFragment()
+        supportFragmentManager.inTransaction {
+            add(R.id.fragment_container, fragment)
+        }
+    }
+
+    private inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
+        val fragmentTransaction = beginTransaction()
+        fragmentTransaction.func()
+        fragmentTransaction.commit()
     }
 }
